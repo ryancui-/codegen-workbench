3186bfec44d1704332e2d6f3cb853957d3fe04ad
ryancui- <cuizhaowei92@gmail.com>
2018-08-28 10:09:18 +0800

    feat: 加入 codemirror 编辑文件

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,29 @@
 new Vue({
   el: '#app'
 })
+
+const editor = CodeMirror(document.getElementById('editor'), {
+  lineNumbers: true,
+  tabSize: 2,
+  lineWrapping: true
+})
+
+editor.setOption('extraKeys', {
+  Tab: cm => {
+    const spaces = Array(cm.getOption('indentUnit') + 1).join(' ')
+    cm.replaceSelection(spaces)
+  }
+})
+
+var zNodes = [
+  {
+    name: "test1", open: true, children: [
+      {name: "test1_1"}, {name: "test1_2"}]
+  },
+  {
+    name: "test2", open: true, children: [
+      {name: "test2_1"}, {name: "test2_2"}]
+  }
+]
+
+zTreeObj = $.fn.zTree.init($("#fileTree"), {}, zNodes)
